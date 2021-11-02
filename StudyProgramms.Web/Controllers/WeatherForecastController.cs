82576bfaf9cf70dbebfde8c553cc9f0eddce29cc
dmitry <shemeldima2@yandex.ru>
2021-11-03 02:43:42 +0300

    Change namespace

--- StudyProgramms.Web/Controllers/WeatherForecastController.cs
+++ StudyProgramms.Web/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace StudyProgramms.Web.Controller
+namespace StudyProgramms.Web.Controllers
 {
     [ApiController]
     [Route("[controller]")]
